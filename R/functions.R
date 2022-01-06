cec2f386439daca865defe20d588c7074cf5e07e
Nathan Chan <nath.chan@mail.utoronto.ca>
2022-01-06 13:56:07 -0500

    Set up project directories, add raw data, and add template scripts.

---
+++ R/functions.R
@@ -0,0 +1,5 @@
+# File name: functions.R
+# Path: './R/functions.R'
+
+# Author: N. Chan
+# Purpose: Script for housing custom functions required for project.
---
+++ scripts/00_init.R
@@ -0,0 +1,5 @@
+# File name: 00_init.R
+# Path: './scripts/00_init.R'
+
+# Author: N. Chan
+# Purpose: Initializes project and installs required dependencies.
